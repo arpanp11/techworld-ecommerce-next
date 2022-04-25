68ecf08315fb237b2d8fa9c06de4c74402a84316
arpanp11 <arpanpatel2810@gmail.com>
2022-04-25 16:08:16 -0400

    connect sanity to frontend

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,35 @@
 import React from 'react';
 
 import { Product, FooterBanner, HeroBanner } from '../components';
+import { client } from '../lib/client';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className='products-container'>
-        {['Product 1 ', 'Product 2 ', 'Product 3 ', 'Product 4 '].map(
-          (product) => product
-        )}
+        {products?.map((product) => product.name)}
       </div>
-
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const products = await client.fetch('*[_type == "product"]');
+  const bannerData = await client.fetch('*[_type == "banner"]');
+
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  };
 };
 
 export default Home;
